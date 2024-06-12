33753ff61a25068bb37cbb5a780e6f5f227bbf6d
VietLQ1 <vietdz145@gmail.com>
2024-06-12 17:24:47 +0700

    fix: gravitational acceleration

--- src/player.ts
+++ src/player.ts
@@ -37,7 +37,7 @@ export class Player extends GameObject {
         }
         if (!this._isOnGround) {
                 this.position[1] -= this._jumpForce * deltaTime;
-                this._jumpForce -= 3000 * deltaTime;
+                this._jumpForce -= 4900 * deltaTime;
 
             if (this.position[1] >= window.innerHeight - this._height) {
                 this.position[1] = window.innerHeight - this._height;
