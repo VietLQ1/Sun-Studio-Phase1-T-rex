f70f36761a792c7d1a03576a2c6fceb024d43b4e
VietLQ1 <vietdz145@gmail.com>
2024-06-12 18:12:47 +0700

    feat: Audio Clip

---
+++ src/components/audioClip.ts
@@ -0,0 +1,17 @@
+export class AudioClip {
+    private _audio: HTMLAudioElement;
+    constructor(clipPath : string) {
+        this._audio = new Audio(clipPath);
+    }
+    play() {
+        this._audio.loop = false;
+        this._audio.play();
+    }
+    playLoop() {
+        this._audio.loop = true;
+        this._audio.play();
+    }
+    pause() {
+        this._audio.pause();
+    }
+}
