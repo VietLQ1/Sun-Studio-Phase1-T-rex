b9533138dcc00a7338cc2e9936908302df017030
VietLQ1 <vietdz145@gmail.com>
2024-06-13 17:18:43 +0700

    fix: index html

--- src/bird.ts
+++ src/bird.ts
@@ -15,7 +15,7 @@ export class Bird extends Obstacle
         this._speed = 10000;
         this._spawnInterval = 3.14159265 * 3;
         this.position[0] = this.position[0] + window.innerWidth + Math.random() * 3000 * this._spawnInterval;
-        this.position[1] = window.innerHeight - this._height - 150 - Math.random() * 100; 
+        this.position[1] = window.innerHeight - this._height - 180; 
         this._animator = new Animator();
         this._animator.setState(new NormalState());
         this._animator.addSprite(new SpriteRenderer('assets/images/bird_flap_0.png'));
